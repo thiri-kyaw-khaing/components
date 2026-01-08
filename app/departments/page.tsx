9c538468c05e411e68ab4d3690f43605ff13a6d6
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-01-08 21:41:00 +0700

    buttonDialog comp

--- app/departments/page.tsx
+++ app/departments/page.tsx
@@ -1,7 +1,22 @@
+import ButtonDialog from "@/components/dashboard/buttonDialog";
+import SubTitle from "@/components/subTitle";
+import Title from "@/components/title";
 import React from "react";
 
 function DepartmentPage() {
-  return <div>DepartmentPage lan edited v2 second time</div>;
+  return (
+    <>
+      <div className="flex items-center m-6 w-full">
+        <div className="space-y-4">
+          <Title text="Department Management" />
+          <SubTitle text="Manage departments, assign managers, and view staff" />
+        </div>
+        <div className="ml-auto">
+          <ButtonDialog name="Add Department" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default DepartmentPage;
---
+++ components/dashboard/buttonDialog.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Plus } from "lucide-react";
+function ButtonDialog({ name }: { name?: string }) {
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger>
+          <button className="bg-[#006022] text-white px-4 py-2 rounded-md  transition">
+            <Plus className="inline-block mr-2 w-4 h-4" />
+            {name}
+          </button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
+
+export default ButtonDialog;
