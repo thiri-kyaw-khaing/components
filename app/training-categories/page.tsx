bc16ed9768c1cdad4bec93bb9b6d927592151694
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-01-09 15:09:51 +0700

    training category

--- app/training-categories/page.tsx
+++ app/training-categories/page.tsx
@@ -1,7 +1,20 @@
+import ButtonDialog from "@/components/dashboard/buttonDialog";
+import PageHeader from "@/components/dashboard/pageHeader";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 function TrainingCategories() {
-  return <div>TrainingCategories chan nyein thu a chaw lay edited</div>;
+  return (
+    <>
+      <div className="m-6 space-y-4">
+        <PageHeader
+          title="Training Categories"
+          subtitle="Manage training categories and classifications"
+          action={<ButtonDialog name="Add Training Category" />}
+        />
+      </div>
+    </>
+  );
 }
 
 export default TrainingCategories;
