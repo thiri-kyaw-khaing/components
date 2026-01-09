09b203beaa65e7be5a806e658e6a15a7eefb19b9
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-01-09 13:33:11 +0700

    dialog

--- components/dashboard/buttonDialog.tsx
+++ components/dashboard/buttonDialog.tsx
@@ -1,32 +1,58 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Plus } from "lucide-react";
 function ButtonDialog({ name }: { name?: string }) {
   return (
     <div>
       <Dialog>
-        <DialogTrigger>
-          <button className="bg-[#006022] text-white px-4 py-2 rounded-md">
-            <Plus className="inline-block mr-2 w-4 h-4" />
-            {name}
-          </button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
+        <form>
+          <DialogTrigger asChild>
+            <Button className="bg-[#006022] text-white px-4 py-2 rounded-md hover:bg-[#005018] ">
+              <Plus className="inline-block mr-2 w-4 h-4" />
+              {name}
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you&apos;re
+                done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4">
+              <div className="grid gap-3">
+                <Label htmlFor="name-1">Name</Label>
+                <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="username-1">Username</Label>
+                <Input
+                  id="username-1"
+                  name="username"
+                  defaultValue="@peduarte"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </form>
       </Dialog>
     </div>
   );
