5a1921d366a81b066cdaead12c8fd2ad6649ba8d
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-01-26 16:30:52 +0700

    select items

--- app/(root)/training-plans/page.tsx
+++ app/(root)/training-plans/page.tsx
@@ -3,6 +3,16 @@ import PageHeader from "@/components/dashboard/pageHeader";
 import CreateTrainingPlan from "@/components/training-plans/createTrainingPlanForm";
 import PlanCard from "@/components/training-plans/planCard";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { courses } from "@/lib/data";
 import { Search } from "lucide-react";
 import React, { act } from "react";
@@ -20,15 +30,52 @@ function TrainingPlans() {
         }
       />
       {/* Search bar */}
-      <div className="border border-[#006022] rounded-lg p-2 flex items-center gap-2 mt-6">
-        <Search className="text-gray-400" />
-        <input
-          type="text"
-          placeholder="Search departments..."
-          className="w-full outline-none border-none focus:ring-0"
-          // value={searchTerm} // ✅ controlled value
-          // onChange={(e) => setSearchTerm(e.target.value)}
-        />
+
+      <div className="flex items-center gap-4 my-6 justify-between">
+        {/* Search */}
+        <div className="relative w-[70%]">
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-4 h-4" />
+          <Input
+            placeholder="Search by name, email, or employee ID..."
+            className="pl-9 border-[#006022]"
+          />
+        </div>
+
+        {/* DepartmentFilter */}
+        <Select>
+          <SelectTrigger className="w-[180px] border-[#006022]">
+            <SelectValue placeholder="All Departments" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>All Departments</SelectLabel>
+              <SelectItem value="apple">Apple</SelectItem>
+              <SelectItem value="banana">Banana</SelectItem>
+              <SelectItem value="blueberry">Blueberry</SelectItem>
+              <SelectItem value="grapes">Grapes</SelectItem>
+              <SelectItem value="pineapple">Pineapple</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        {/* Category Filter */}
+        <Select>
+          <SelectTrigger className="w-[180px] border-[#006022]">
+            <SelectValue placeholder="All Categories" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="1">สนับสนุนนโยบายสิ่งแวดล้อม</SelectItem>
+            <SelectItem value="2">ความปลอดภัยและอาชีวอนามัย</SelectItem>
+            <SelectItem value="3">ความปลอดภัยและอาชีวอนามัย</SelectItem>
+            <SelectItem value="4">ความปลอดภัยและอาชีวอนามัย</SelectItem>
+            <SelectItem value="5">ความปลอดภัยและอาชีวอนามัย</SelectItem>
+            <SelectItem value="6">ความปลอดภัยและอาชีวอนามัย</SelectItem>
+
+            <SelectItem value="suspended">Suspended</SelectItem>
+          </SelectContent>
+        </Select>
+
+        {/* Button */}
+        <Button className="bg-[#006022] hover:bg-[#005018] px-8">Search</Button>
       </div>
       <div className="space-y-6">
         {courses.map((plan) => (
