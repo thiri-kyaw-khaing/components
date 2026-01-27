2e86fe97f2d917a8a1ad518526347a6ef92b3912
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-01-27 22:00:36 +0700

    table scroll

--- components/training-records/TrainingRecordTable.tsx
+++ components/training-records/TrainingRecordTable.tsx
@@ -18,43 +18,45 @@ import { colors } from "@/lib/color";
 
 function TrainingRecordTable() {
   return (
-    <div className="overflow-x-auto">
-      <Table className="min-w-[1200px] table-auto whitespace-nowrap rounded-t-md">
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader className={`bg-[${colors.secondary}] rounded-t-md`}>
-          <TableRow>
-            <TableHead className="w-[200px] font-bold">Course Code</TableHead>
-            <TableHead className="w-[200px] font-bold">Course Name</TableHead>
-            <TableHead className="w-[250px] font-bold">Location</TableHead>
-            <TableHead className="text-right w-[300px] font-bold">
-              Department
-            </TableHead>
-            <TableHead className="text-right w-[150px] font-bold">
-              Role
-            </TableHead>
+    <div className="relative w-full">
+      <div className="overflow-x-auto">
+        <Table className="min-w-[1200px] table-auto whitespace-nowrap rounded-t-md">
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader className={`bg-[${colors.secondary}] rounded-t-md`}>
+            <TableRow>
+              <TableHead className="w-[200px] font-bold">Course Code</TableHead>
+              <TableHead className="w-[200px] font-bold">Course Name</TableHead>
+              <TableHead className="w-[250px] font-bold">Location</TableHead>
+              <TableHead className="text-right w-[300px] font-bold">
+                Employee ID
+              </TableHead>
+              <TableHead className="text-right w-[150px] font-bold">
+                Name-Surname
+              </TableHead>
 
-            <TableHead className="text-right w-[150px] font-bold">
-              Actions
-            </TableHead>
-            <TableHead className="text-right w-[150px] font-bold">
-              Actions
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {users.map((user) => (
-            <TableRow key={user.id}>
-              <TableCell className="font-medium">{user.id}</TableCell>
-              <TableCell>{user.name}</TableCell>
-              <TableCell>hi</TableCell>
-              <TableCell className="text-right">
-                {user.department.name}
-              </TableCell>
-              <TableCell className="text-right">{user.role}</TableCell>
+              <TableHead className="text-right w-[150px] font-bold">
+                Actions
+              </TableHead>
+              <TableHead className="text-right w-[150px] font-bold">
+                Actions
+              </TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell className="font-medium">{user.id}</TableCell>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>hi</TableCell>
+                <TableCell className="text-right">
+                  {user.department.name}
+                </TableCell>
+                <TableCell className="text-right">{user.role}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
