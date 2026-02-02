718908d6610f46cfd20d455aec4a6db1c17a7bd0
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2026-02-03 00:03:12 +0700

    login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -14,6 +14,12 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import Logo from "@/components/login/logo";
+import {
+  Dialog,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 const formSchema = z.object({
   email: z.string().min(2).max(50),
@@ -34,49 +40,107 @@ function LoginForm() {
   }
   return (
     <>
-      <Logo />
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <div className="grid gap-4 mt-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter Email" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
+      <div className="flex flex-col space-y-4">
+        <Logo />
+        <h1 className="text-[#4A5565]">
+          Training and Record Management System
+        </h1>
+        <div className="border rounded-md p-6">
+          {/* <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="grid gap-4 mt-4">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-          <div className="grid gap-4 mt-4">
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter Password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
+              <div className="grid gap-4 mt-4">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter Password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
 
-          <Button
-            type="submit"
-            className="bg-[#006022] text-white hover:bg-[#005018]"
-          >
-            Login
-          </Button>
-        </form>
-      </Form>
+              <Button
+                type="submit"
+                className="bg-[#006022] text-white hover:bg-[#005018] mt-6 w-full"
+              >
+                Login
+              </Button>
+            </form>
+          </Form> */}
+          <Dialog>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <DialogHeader>
+                  <DialogTitle>Add New Department</DialogTitle>
+                </DialogHeader>
+
+                <div className="grid gap-4 mt-4">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email Address</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter Department Name"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Enter Password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
+                <DialogFooter className="mt-6">
+                  <Button
+                    type="submit"
+                    className="bg-[#006022] text-white hover:bg-[#005018] w-full"
+                  >
+                    Add Department
+                  </Button>
+                </DialogFooter>
+              </form>
+            </Form>
+          </Dialog>
+        </div>
+      </div>
     </>
   );
 }
